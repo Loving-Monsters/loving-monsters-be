8918aaed8cb1b6b8734ccc669bb7248d71af31a6
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-03-01 16:52:37 -0800

    paul-branch

--- server.js
+++ server.js
@@ -6,16 +6,3 @@ http.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
 });
 
-// const pool = require('./lib/utils/pool');
-
-// const PORT = process.env.PORT || 7890;
-
-// app.listen(PORT, () => {
-//   // eslint-disable-next-line no-console
-//   console.log(`Started on ${PORT}`);
-// });
-
-// process.on('exit', () => {
-//   console.log('Goodbye!');
-//   pool.end();
-// });
