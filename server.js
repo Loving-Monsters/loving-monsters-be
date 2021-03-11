f1ccd5b675352aa4c67194d0a2f00aa8b935a07c
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-03-11 14:30:24 -0800

    removed unnecessary spaces and console.logs

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const { http } = require('./lib/app');
 const PORT = process.env.PORT || 3000;
 
 http.listen(PORT, () => {
-  console.log(`listening on ${PORT}`);
+    console.log(`listening on ${PORT}`);
 });
 
